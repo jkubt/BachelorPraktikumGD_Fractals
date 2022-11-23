e520a3bdb3f99827d70a3dd6cf9de6f934ff1c6a
jkubt <jan.koerber@uni-bayreuth.de>
2022-11-23 15:31:59 +0100

    add glew and finish initialization

--- FractalTest/FractalTest/main.cpp
+++ FractalTest/FractalTest/main.cpp
@@ -1,20 +1,33 @@
 #include <iostream>
 #define SDL_MAIN_HANDLED
 #include <SDL.h>
+#define GLEW_STATIC
+#include <GL/glew.h>
 
 #pragma comment(lib,"SDL2.lib")
+#pragma comment(lib, "glew32s.lib")
 #pragma comment(lib,"opengl32.lib")
 
 int main(int argc, char** argv){
 	SDL_Window* window;
 	SDL_Init(SDL_INIT_EVERYTHING);
 
-	window = SDL_CreateWindow("FractalTest", SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED, 800, 600, SDL_WINDOW_OPENGL);
+	SDL_GL_SetAttribute(SDL_GL_RED_SIZE,8);
+	SDL_GL_SetAttribute(SDL_GL_GREEN_SIZE, 8);
+	SDL_GL_SetAttribute(SDL_GL_BLUE_SIZE, 8);
+	SDL_GL_SetAttribute(SDL_GL_ALPHA_SIZE, 8);
+	SDL_GL_SetAttribute(SDL_GL_BUFFER_SIZE, 32);
+	SDL_GL_SetAttribute(SDL_GL_DOUBLEBUFFER, 1);
+
+	window = SDL_CreateWindow("FractalTest", SDL_WINDOWPOS_UNDEFINED, SDL_WINDOWPOS_UNDEFINED, 800, 600, SDL_WINDOW_OPENGL);
 	SDL_GLContext glContext = SDL_GL_CreateContext(window);
 
 	bool close = false;
 	while (!close) {
 
+		glClearColor(0.0f,0.0f,0.0f,1.0f);
+		glClear(GL_COLOR_BUFFER_BIT);
+		
 		SDL_GL_SwapWindow(window);
 
 		SDL_Event event;
